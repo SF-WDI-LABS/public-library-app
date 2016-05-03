e8b610dae56a66f1a8de2b3d0c0aeb518f3f9f12
relative-rene <rene.arellano415@gmail.com>
2016-05-03 14:49:22 -0700

    final faze checking links

--- lib_app/app/controllers/users_controller.rb
+++ lib_app/app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
   def create
     user_params = params.require(:user).permit(:first_name, :last_name, :email, :password)
     @user = User.create(user_params)
+    login(@user)
 
     redirect_to root_path
   end
--- lib_app/config/routes.rb
+++ lib_app/config/routes.rb
@@ -8,6 +8,17 @@ Rails.application.routes.draw do
   post "/users", to: "users#create"
 
   get "/users/:id", to: "users#show", as: "user"
+
+
+
+
+
+  get "/login", to: "sessions#new"
+
+  get "/logout", to: "sessions#destroy"
+  post "/sessions", to:"sessions#create"
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
