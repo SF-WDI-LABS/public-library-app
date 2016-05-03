87d88bd57d5f86223dea64b1ddd3969622f45b7d
relative-rene <rene.arellano415@gmail.com>
2016-05-03 14:21:49 -0700

    vew create new controller added with erb's

--- lib_app/config/routes.rb
+++ lib_app/config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  root to: "users#index"
+
+  get "/users", to: "users#index", as: "users"
+
+  get "/users/new", to: "users#new", as: "new_user"
+
+  post "/users", to: "users#create"
+
+  get "/users/:id", to: "users#show", as: "user"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
